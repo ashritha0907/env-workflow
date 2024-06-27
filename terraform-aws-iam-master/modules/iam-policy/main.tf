2f91aa08ff19d24c5b85a6b8498959d126903fb2
ASHRITHA  KASULA <c-ASHRITHAKA@ad.crisil.com>
2024-06-27 13:24:56 +0530

    aws

---
+++ terraform-aws-iam-master/modules/iam-policy/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_iam_policy" "policy" {
+  count = var.create_policy ? 1 : 0
+
+  name        = var.name
+  #name_prefix = var.name_prefix
+  path        = var.path
+  description = var.description
+
+  policy = var.policy
+
+  tags = var.tags
+}
